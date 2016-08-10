d52129b4c91afd2dfedd2a4adbd1689f74c520f8
dusan.vesic <dusan.vesic@gmail.com>
2016-08-10 09:01:52 +0200

    Init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import _ from 'lodash'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        App
+      </div>
+    );
+  }
+}
+
+export default App;
